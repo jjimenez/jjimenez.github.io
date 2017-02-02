6f8f373b72b0ebbac618f2470bfa46f71c209732
Jose Jimenez <jose-jimenez@uiowa.edu>
2017-02-01 22:01:30 -0600

    adding javascript haiku generator

--- js/shared.js
+++ js/shared.js
@@ -40,6 +40,8 @@ var config = {
     amplitudeMax: 300,     // what the maximum amplitude might be
     amplitudeMin: 0.0,     // what the minimum amplitude might be
     transparency: 0.75,   // the transparency of the circles
+    drawComplex: false,   // use the complex shape
+    complexScaler: 3,    // how much bigger complex shape is than circles
     useGrid: true,
     addScaled: false,     // copy the scaled image to the top corner for each cycle.
     // functions to calculate various things below including the wobble of the positioning of the circles,
@@ -78,6 +80,22 @@ var config = {
     },
     randomColor: function () {
         return '#' + Math.floor(Math.random() * 16777215).toString(16);
+    },
+    center: function(arr, centerX, centerY) {
+        new_arr = [];
+        arr_len = arr.length;
+        idx = 0;
+        while (idx < arr_len-1) {
+            new_arr[idx] = Math.floor(centerX - arr[idx]);
+            new_arr[idx+1] = Math.floor(centerY - arr[idx+1]);
+            idx = idx + 2;
+        }
+        return new_arr;
+    },
+    scale_a: function(arr, radius) {
+        return $(arr).map(function(i,n){
+            return Math.floor(n * radius);
+        });
     }
 }
 
@@ -174,7 +192,13 @@ function drawTimeDomain() {
 
     var radius = config.radiusFromAmplitudeArray(amplitudeArray);
 
-    drawCircle(centerX, centerY, radius, config.randomColor());
+    if (config.drawComplex) {
+        drawComplexShape(centerX, centerY, radius, config.randomColor());
+    }
+    else {
+        drawCircle(centerX, centerY, radius, config.randomColor());
+    }
+
 
 
     // loop around the canvas when we reach the end
@@ -200,6 +224,52 @@ function drawCircle(centerX, centerY, radius, fillStyle) {
     ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
     ctx.fill();
     ctx.fillStyle = saveStyle;
+}
+
+function drawComplexShape(centerX, centerY, radius, fillStyle) {
+    radius = radius * config.complexScaler;
+    var saveStyle = ctx.fillStyle;
+    ctx.fillStyle = fillStyle;
+    var start = [0.595,0.1];
+    var steps = [
+        [0.795,0,0.795,-0.25,0.295,-0.25],
+        [0.195,-0.4,-0.155,-0.4,-0.255,-0.25],
+        [-0.655,-0.25,-0.655,-0.1,-0.505,0],
+        [-0.705,0.3,-0.405,0.35,-0.255,0.25],
+        [-0.155,0.475,0.195,0.4,0.195,0.25],
+        [0.445,0.475,0.695,0.4,0.595,0.1],
+        [0.795,0.475,0.795,0.4,0.595,0.25],
+        [-0.705,-0.4,-0.655,-0.4,-0.505,-0.25]
+    ];
+    var scaled_steps = steps;
+    var scaled_steps = $(scaled_steps).map(function(i,n) {
+        return config.scale_a(n,radius);
+    } );
+
+    var scaled_start = config.scale_a(start,radius);
+    var centered_start = config.center(scaled_start, centerX, centerY);
+
+    var centered_steps = $(scaled_steps).map(function(i,n) {
+        return [config.center(n,centerX, centerY)];
+    });
+
+
+    ctx.beginPath();
+
+    ctx.moveTo(centered_start[0], centered_start[1]);
+    ctx.bezierCurveTo(centered_steps[0][0],centered_steps[0][1],centered_steps[0][2],centered_steps[0][3],centered_steps[0][4],centered_steps[0][5]);
+    ctx.bezierCurveTo(centered_steps[1][0],centered_steps[1][1],centered_steps[1][2],centered_steps[1][3],centered_steps[1][4],centered_steps[1][5]);
+    ctx.bezierCurveTo(centered_steps[2][0],centered_steps[2][1],centered_steps[2][2],centered_steps[2][3],centered_steps[2][4],centered_steps[2][5]);
+    ctx.bezierCurveTo(centered_steps[3][0],centered_steps[3][1],centered_steps[3][2],centered_steps[3][3],centered_steps[3][4],centered_steps[3][5]);
+    ctx.bezierCurveTo(centered_steps[4][0],centered_steps[4][1],centered_steps[4][2],centered_steps[4][3],centered_steps[4][4],centered_steps[4][5]);
+    ctx.bezierCurveTo(centered_steps[5][0],centered_steps[5][1],centered_steps[5][2],centered_steps[5][3],centered_steps[5][4],centered_steps[5][5]);
+
+
+    ctx.closePath();
+
+    ctx.fill();
+    ctx.fillStyle = saveStyle;
+
 }
 
 
